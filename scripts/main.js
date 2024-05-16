dffbd3ff850f7509d3856c972f187714d0fde1b9
Ignacio Ucedo <ig.ucedo@gmail.com>
2024-05-15 22:12:04 -0300

    agrega conexión por flechas desde script

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ class LienzoInfinito {
     this.contexto = null;
     this.tamanoCelda = tamanoCelda;
     this.escala = 1;
-    this.desplazamientoX = 0;
-    this.desplazamientoY = 0;
+    this.desplazamientoCanvasX = 0;
+    this.desplazamientoCanvasY = 0;
     this.gestorTarjetas = new GestorTarjetas(); 
 
     const lienzo = document.getElementById("canvas");
@@ -38,12 +38,14 @@ class LienzoInfinito {
 
   }
 
-  aVirtualX(xReal) {
-    return (xReal + this.desplazamientoX) * this.escala;
+  //convierte un punto del canvas a un punto en la pantalla (coordenada x)
+  aVirtualX(xCanvas) {
+    return (xCanvas+ this.desplazamientoCanvasX) * this.escala;
   }
 
-  aVirtualY(yReal) {
-    return (yReal + this.desplazamientoY) * this.escala;
+  //convierte un punto del canvas a un punto en la pantalla (coordenada y)
+  aVirtualY(yCanvas) {
+    return (yCanvas + this.desplazamientoCanvasY) * this.escala;
   }
 
   zoom(factor) {
@@ -63,26 +65,6 @@ class LienzoInfinito {
   }
   
 
-  desplazamientoIzquierda(cantidad) {
-    this.desplazamientoX += cantidad;
-    this.dibujar();
-  }
-
-  desplazamientoDerecha(cantidad) {
-    this.desplazamientoX -= cantidad;
-    this.dibujar();
-  }
-
-  desplazamientoArriba(cantidad) {
-    this.desplazamientoY += cantidad;
-    this.dibujar();
-  }
-
-  desplazamientoAbajo(cantidad) {
-    this.desplazamientoY -= cantidad;
-    this.dibujar();
-  }
-
   dibujar() {
     if (this.lienzo && this.contexto) {
       this.lienzo.width = document.body.clientWidth;
@@ -94,8 +76,8 @@ class LienzoInfinito {
 
   dibujarCuadricula() {
     if (this.lienzo && this.contexto) {
-      this.contexto.strokeStyle = "#e1e1e1";
-      this.contexto.lineWidth = 1;
+      this.contexto.strokeStyle = "#5c5c5c";
+      this.contexto.lineWidth = 0.2;
       this.contexto.beginPath();
 
       const ancho = this.lienzo.clientWidth;
@@ -105,13 +87,13 @@ class LienzoInfinito {
       const mitadAncho = ancho / 2;
       let n = Math.floor(mitadAncho / (this.tamanoCelda * this.escala));
 
-      for (let x = ((n) * this.tamanoCelda + this.desplazamientoX) * this.escala; x >= 0; x -= this.tamanoCelda * this.escala ) {
+      for (let x = ((n) * this.tamanoCelda + this.desplazamientoCanvasX) * this.escala; x >= 0; x -= this.tamanoCelda * this.escala ) {
         const origen = x;
         this.contexto.moveTo(origen, 0); //origen de línea
         this.contexto.lineTo(origen, alto); //destino de línea
       }
 
-      for (let x = ((n + 1) * this.tamanoCelda + this.desplazamientoX) * this.escala; x <= ancho; x += this.tamanoCelda * this.escala) {
+      for (let x = ((n + 1) * this.tamanoCelda + this.desplazamientoCanvasX) * this.escala; x <= ancho; x += this.tamanoCelda * this.escala) {
         const origen = x;
         this.contexto.moveTo(origen, 0);
         this.contexto.lineTo(origen, alto);
@@ -120,13 +102,13 @@ class LienzoInfinito {
       //dibujo de horizontales
       const mitadAlto = alto / 2;
       n = Math.floor(mitadAlto / (this.tamanoCelda * this.escala));
-      for (let y = ((n) * this.tamanoCelda + this.desplazamientoY) * this.escala; y >= 0; y -= this.tamanoCelda * this.escala ) {
+      for (let y = ((n) * this.tamanoCelda + this.desplazamientoCanvasY) * this.escala; y >= 0; y -= this.tamanoCelda * this.escala ) {
         const destino = y;
         this.contexto.moveTo(0, destino);
         this.contexto.lineTo(ancho, destino);
       }
 
-      for (let y = ((n + 1) * this.tamanoCelda + this.desplazamientoY) * this.escala; y <= ancho; y += this.tamanoCelda * this.escala) {
+      for (let y = ((n + 1) * this.tamanoCelda + this.desplazamientoCanvasY) * this.escala; y <= ancho; y += this.tamanoCelda * this.escala) {
         const destino = y;
         this.contexto.moveTo(0, destino);
         this.contexto.lineTo(ancho, destino);
@@ -142,6 +124,21 @@ class LienzoInfinito {
   dibujarTarjetas() {
     this.contexto.font = "12px Arial";
 
+  for (const flecha of this.gestorTarjetas.flechas) {
+      const origenVirtualX = this.aVirtualX(flecha.origen.x);
+      const origenVirtualY= this.aVirtualY(flecha.origen.y);
+      const destinoVirtualX = this.aVirtualX(flecha.destino.x);
+      const destinoVirtualY= this.aVirtualY(flecha.destino.y);
+      this.contexto.beginPath();
+      this.contexto.strokeStyle = flecha.color;
+      this.contexto.lineWidth = flecha.grosor;
+      this.contexto.moveTo(origenVirtualX, origenVirtualY);
+      this.contexto.lineTo(destinoVirtualX, destinoVirtualY);
+      this.contexto.stroke();
+      
+    }
+
+
     for (const tarjeta of this.gestorTarjetas.obtenerTodas()) {
         const virtualX = this.aVirtualX(tarjeta.x);
         const virtualY = this.aVirtualY(tarjeta.y);
@@ -150,43 +147,46 @@ class LienzoInfinito {
         this.contexto.shadowColor = "rgba(0, 0, 0, 0.1)";
 
         if (tarjeta.resaltada && (this.tarjetaArrastrada == null || this.tarjetaArrastrada == tarjeta)) {
-          this.contexto.shadowColor = "rgba(100, 180, 255, 0.2)";
+          this.contexto.shadowColor = "rgba(100, 200, 255, 0.3)";
         }
         
         if (tarjeta.resaltada && (this.tarjetaArrastrada == null || this.tarjetaArrastrada == tarjeta)) {
-          this.contexto.save(); 
           this.contexto.strokeStyle = "#40b3f2"; 
-          this.contexto.lineWidth = 3; 
+          this.contexto.lineWidth = 4; 
           this.contexto.beginPath();
-          this.rectanguloRedondeado(this.contexto, virtualX, virtualY, tamanoEscalar, tamanoEscalar, 10);
+          this.rectanguloRedondeado(this.contexto, virtualX, virtualY, tamanoEscalar, tamanoEscalar, tamanoEscalar/10);
           this.contexto.stroke();
-          this.contexto.restore(); 
         }
-        
-        // mouse en zona tolerante
-        this.contexto.fillStyle = tarjeta.mouseEnZonaTolerante && this.tarjetaArrastrada == null ? "#dff4f5" : "#cbeff1"; // Cambia color de fondo
+       
+        this.contexto.fillStyle = "#cbeff1"
 
-        this.rectanguloRedondeado(this.contexto, virtualX, virtualY, tamanoEscalar, tamanoEscalar, 10);
+        // mouse en zona tolerante
+        this.rectanguloRedondeado(this.contexto, virtualX, virtualY, tamanoEscalar, tamanoEscalar, tamanoEscalar/10);
         this.contexto.fill();
 
         // Agregar los puntos en el borde si el mouse está en la zona tolerante
         if (tarjeta.mouseEnZonaTolerante && this.tarjetaArrastrada == null) {
-          const radio = 3;
-          this.contexto.strokeStyle = "#40b3f2"; // color de las circunferencias
-          this.contexto.lineWidth = 2; // grosor del borde
+          const radio = 5;
+          this.contexto.fillStyle = "rgba(0, 165, 240, 0.7)";
+          this.contexto.strokeStyle = "#00a5ff"; // color de las circunferencias
+          this.contexto.lineWidth = 1; // grosor del borde
 
           this.contexto.beginPath();
           this.contexto.arc(virtualX + tamanoEscalar / 2, virtualY , radio, 0, Math.PI * 2); // Punto arriba
           this.contexto.stroke();
+          this.contexto.fill();
           this.contexto.beginPath();
           this.contexto.arc(virtualX + tamanoEscalar, virtualY + tamanoEscalar / 2, radio, 0, Math.PI * 2); // Punto derecha
           this.contexto.stroke();
+          this.contexto.fill();
           this.contexto.beginPath();
           this.contexto.arc(virtualX + tamanoEscalar / 2, virtualY + tamanoEscalar , radio, 0, Math.PI * 2); // Punto abajo
           this.contexto.stroke();
+          this.contexto.fill();
           this.contexto.beginPath();
           this.contexto.arc(virtualX , virtualY + tamanoEscalar / 2, radio, 0, Math.PI * 2); // Punto izquierda
           this.contexto.stroke();
+          this.contexto.fill();
         }
 
         this.contexto.shadowBlur = 0;
@@ -197,23 +197,23 @@ class LienzoInfinito {
 
 
 //  rectángulo con bordes redondeados
-rectanguloRedondeado(ctx, x, y, ancho, alto, radio) {
-    ctx.beginPath();
-    ctx.moveTo(x + radio, y);
-    ctx.lineTo(x + ancho - radio, y);
-    ctx.arcTo(x + ancho, y, x + ancho, y + radio, radio);
-    ctx.lineTo(x + ancho, y + alto - radio);
-    ctx.arcTo(x + ancho, y + alto, x + ancho - radio, y + alto, radio);
-    ctx.lineTo(x + radio, y + alto);
-    ctx.arcTo(x, y + alto, x, y + alto - radio, radio);
-    ctx.lineTo(x, y + radio);
-    ctx.arcTo(x, y, x + radio, y, radio);
-    ctx.closePath();
+rectanguloRedondeado(contexto, x, y, ancho, alto, radio) {
+    contexto.beginPath();
+    contexto.moveTo(x + radio, y);
+    contexto.lineTo(x + ancho - radio, y);
+    contexto.arcTo(x + ancho, y, x + ancho, y + radio, radio);
+    contexto.lineTo(x + ancho, y + alto - radio);
+    contexto.arcTo(x + ancho, y + alto, x + ancho - radio, y + alto, radio);
+    contexto.lineTo(x + radio, y + alto);
+    contexto.arcTo(x, y + alto, x, y + alto - radio, radio);
+    contexto.lineTo(x, y + radio);
+    contexto.arcTo(x, y, x + radio, y, radio);
+    contexto.closePath();
 }
 
   agregarTarjeta(x, y, texto) {
-    const xEscalar = x / this.escala - this.desplazamientoX;
-    const yEscalar = y / this.escala - this.desplazamientoY;
+    const xEscalar = x / this.escala - this.desplazamientoCanvasX;
+    const yEscalar = y / this.escala - this.desplazamientoCanvasY;
     this.gestorTarjetas.agregar(xEscalar, yEscalar, texto);
     this.dibujar();
   }
@@ -260,7 +260,6 @@ rectanguloRedondeado(ctx, x, y, ancho, alto, radio) {
   }
 
   manejarMouseMove(evento) {
-  
     //arrastres
     if (this.tarjetaArrastrada != null) {
       // si tieen clickada una tarjetaa, mover la tarjeta en lugar de mover el lienzo
@@ -274,15 +273,23 @@ rectanguloRedondeado(ctx, x, y, ancho, alto, radio) {
       this.tarjetaArrastrada.y += deltaYEscalar;
       this.inicioArrastreX = evento.clientX;
       this.inicioArrastreY = evento.clientY;
-      //this.dibujar();
-      this.dibujarTarjetas();
-    } else if (this.arrastrandoEnCanvas) {
+      const conexionesDeTarjetaArrastrada = this.gestorTarjetas.conexiones.get(this.tarjetaArrastrada);
+      if (conexionesDeTarjetaArrastrada){
+        for (const conexion of conexionesDeTarjetaArrastrada){
+          let puntoDeConexion = conexion[1];
+          puntoDeConexion.x += deltaXEscalar;
+          puntoDeConexion.y += deltaYEscalar;
+
+        }
+      }
+        this.dibujarTarjetas();
+      } else if (this.arrastrandoEnCanvas) {
       //cuando estoy arrastrando el canvas
 
       const deltaX = evento.clientX - this.inicioArrastreX;
       const deltaY = evento.clientY - this.inicioArrastreY;
-      this.desplazamientoX += deltaX / this.escala;
-      this.desplazamientoY += deltaY / this.escala;
+      this.desplazamientoCanvasX+= deltaX /this.escala;
+      this.desplazamientoCanvasY+= deltaY /this.escala;
       this.inicioArrastreX = evento.clientX;
       this.inicioArrastreY = evento.clientY;
       this.dibujar();
@@ -375,19 +382,18 @@ rectanguloRedondeado(ctx, x, y, ancho, alto, radio) {
     );
   }
 
-
 }
-
 
 class GestorTarjetas {
   constructor() {
     this.tarjetas = [];
+    this.flechas = [];
+    this.conexiones = new Map();
   }
  
   agregar(x, y, texto) {
     const tarjeta = new Tarjeta(x, y, texto);
     this.tarjetas.push(tarjeta);
-    //this.tarjetas.unshift(tarjeta);
   }
   
   limpiar() {
@@ -402,6 +408,57 @@ class GestorTarjetas {
     this.tarjetas.splice(indice, 1);
   }
 
+  conectarTarjetas(padre, hijo, ladoPadre, ladoHijo, escalaCanvas, contexto) {
+    const puntoDeConexionPadre = this.obtenerPuntoDeConexion(padre, ladoPadre, escalaCanvas);  
+    const puntoDeConexionHijo = this.obtenerPuntoDeConexion(hijo, ladoHijo, escalaCanvas);  
+    const flecha = new Flecha(puntoDeConexionPadre, puntoDeConexionHijo);
+    flecha.dibujar(contexto);
+    this.flechas.push(flecha);
+
+
+    if (!this.conexiones.has(padre)) {
+        this.conexiones.set(padre, [[flecha, puntoDeConexionPadre]]);
+    } else {
+        this.conexiones.set(padre, this.conexiones.get(padre).push([flecha, puntoDeConexionPadre]));
+    }
+
+
+    if (!this.conexiones.has(hijo)) {
+        this.conexiones.set(hijo, [[flecha, puntoDeConexionHijo]]);
+    } else {
+        this.conexiones.set(hijo, this.conexiones.get(hijo).push([flecha, puntoDeConexionHijo]));
+    }
+
+}
+
+
+  obtenerPuntoDeConexion(tarjeta, lado, escalaCanvas){
+    let punto = {};
+    if (lado =="arriba"){
+      let xRelativo = (tarjeta.tamanoOriginal * escalaCanvas)/2;
+      let yRelativo = 0; 
+      punto = {x: xRelativo + tarjeta.x,y: yRelativo + tarjeta.y}
+
+    } else if (lado == "derecha"){
+      let xRelativo = (tarjeta.tamanoOriginal * escalaCanvas)/2;
+      let yRelativo = (tarjeta.tamanoOriginal * escalaCanvas)/2; 
+      punto = {x: xRelativo + tarjeta.x,y: yRelativo + tarjeta.y}
+
+    } else if (lado == "abajo"){
+      let xRelativo = (tarjeta.tamanoOriginal * escalaCanvas)/2;
+      let yRelativo = (tarjeta.tamanoOriginal * escalaCanvas);
+      punto = {x: xRelativo + tarjeta.x,y: yRelativo + tarjeta.y}
+
+    } else if (lado == "izquierda"){
+      let xRelativo = 0; 
+      let yRelativo = (tarjeta.tamanoOriginal * escalaCanvas)/2;
+      punto = {x: xRelativo + tarjeta.x,y: yRelativo + tarjeta.y}
+
+    }
+
+   return punto; 
+  }
+
   acomodarTarjetaAlFinal(tarjeta){
     let lista = this.tarjetas;
     const indicetarjeta = lista.indexOf(tarjeta);
@@ -410,9 +467,13 @@ class GestorTarjetas {
       lista.push(tarjeta); // Agregar el tarjeta al final de la lista
     }
     this.tarjetas = lista;      
-    
   }
 
+  dibujarFlechas(contexto){
+    for (const flecha of this.flechas){
+      flecha.dibujar(contexto);
+    }
+  }
 }
 
 
@@ -425,13 +486,13 @@ class Tarjeta {
     this.mouseEnZonaTolerante = false; 
     this.resaltada = false;
 
-    this.tamanoOriginal= 100;
+    this.tamanoOriginal= 115;
     this.grosorZonaTolerancia = 15;
   }
 }
 
 class Flecha {
-  constructor(origen, destino, color = '#000000', grosor = 2) {
+  constructor(origen, destino, color = 'rgba(100, 190, 255, 1)', grosor = 2) {
     this.origen = origen;
     this.destino = destino;
     this.color = color;
@@ -447,9 +508,6 @@ class Flecha {
     contexto.stroke();
   }
 
-  actualizarDestino(destino) {
-    this.destino = destino;
-  }
 }
 
 function obtenerEnteroAleatorio(min, max) {
@@ -469,6 +527,13 @@ document.addEventListener("contextmenu", (e) => e.preventDefault(), false);
 
 document.getElementById("add-card").addEventListener("click", () => lienzoInfinito.agregarTarjeta(obtenerEnteroAleatorio(100, 300), obtenerEnteroAleatorio(100, 300), "Hola"));
 
-//prueba flechac
-let flechaa = new Flecha({ x: 100, y: 100 }, { x: 200, y: 200 }, "red", 1);
-flechaa.dibujar(lienzoInfinito.lienzo.getContext('2d'));
+lienzoInfinito.agregarTarjeta(obtenerEnteroAleatorio(100, 300), obtenerEnteroAleatorio(100, 300), "Hola");
+lienzoInfinito.agregarTarjeta(obtenerEnteroAleatorio(100, 300), obtenerEnteroAleatorio(100, 300), "Hola");
+//lienzoInfinito.agregarTarjeta(obtenerEnteroAleatorio(100, 300), obtenerEnteroAleatorio(100, 300), "Hola");
+//lienzoInfinito.agregarTarjeta(obtenerEnteroAleatorio(100, 300), obtenerEnteroAleatorio(100, 300), "Hola");
+let padre = lienzoInfinito.gestorTarjetas.tarjetas[0];
+let hijo = lienzoInfinito.gestorTarjetas.tarjetas[1];
+lienzoInfinito.gestorTarjetas.conectarTarjetas(padre,hijo, "abajo", "arriba", lienzoInfinito.escala, lienzoInfinito.contexto);
+//padre = lienzoInfinito.gestorTarjetas.tarjetas[2];
+//hijo = lienzoInfinito.gestorTarjetas.tarjetas[3];
+//lienzoInfinito.gestorTarjetas.conectarTarjetas(padre,hijo, "abajo", "arriba", lienzoInfinito.escala, lienzoInfinito.contexto);
